64463b1e46a5d8ca18edb3bdaed572ea219e7db0
2669867336 <2669867336@qq.com>
2021-01-28 09:09:32 +0800

    考试模块

--- bootstrap-table/js/kaoshixiangqing.js
+++ bootstrap-table/js/kaoshixiangqing.js
@@ -9,7 +9,7 @@ function reLoad(){
 function load(){
     $("#table").bootstrapTable({
         // url:"http://192.168.43.189:8080/Exam_Jay_SSM/selectEx",
-        url:'http://localhost:8080/Exam_Jay_SSM/',
+        url:'http://localhost:8080/Exam_Jay_SSM/selectGradeAll',
         method:"POST",
         dataType:"JSON",
         striped : true, //是否显示行间隔色
@@ -48,23 +48,32 @@ function load(){
             // },
             {
                 title:'学号',
-                field:'examinationType'
+                field:'gradeLoginid'
 
             },
             {
-                title:'试题内容',
-                field:'examinationTitle',
+                title:'姓名',
+                field:'gradeName',
 
             },
             {
-                title:'难度系数',
-                field:'examinationDegree'
+                title:'得分',
+                field:'gradeScore'
              },
             {
-                title:'添加时间',
-                field:'createtime'
+                title:'正确率',
+                field:'gradeAccuracy'
             }
              ,
+            {
+                title:'登录时间',
+                field:'loginTime'
+            }
+            ,{
+                title:'交卷时间',
+                field:'finishTime'
+            }
+            ,
             {
                 title:'操作',
                 // field:'createTime', //和后台pojo属性进行对应
@@ -73,114 +82,23 @@ function load(){
                 width:'150px',//设置列宽
                 formatter:function(value,row,index){
                     //如果将来 涉及到字符串数据传入参数  需要设置单引号
-                    let d='<a href="javascript:void(0);" onclick="removeData(\''+row.examinationTitle+'\')">删除</a>'
-                    let m='<a href="javascript:void(0);" onclick="modifyGoods(\''+row.examinationTitle+'\',\''
-                        +row.examinationA+'\',\''+row.examinationB+'\',\''+row.examinationC+'\',\''+row.examinationD+'\',\''
-                        +row.examinationAnswer+'\',\''+row.examinationScore+'\',\''+row.examinationDegree+'\',\''+row.examinationType+'\',\''+row.examinationId+'\')">修改</a>'
+                    let d='<a href="javascript:void(0);" onclick="lookData(\''+row.gradeName+'\',\''+row.paperId+'\',\''+row.gradeLoginid+'\',\''+row.gradeScore+'\',\''+row.gradeAccuracy+'\')">阅卷详情</a>'
+                    // let m='<a href="javascript:void(0);" onclick="modifyGoods(\''+row.examinationTitle+'\',\''
+                    //     +row.examinationA+'\',\''+row.examinationB+'\',\''+row.examinationC+'\',\''+row.examinationD+'\',\''
+                    //     +row.examinationAnswer+'\',\''+row.examinationScore+'\',\''+row.examinationDegree+'\',\''+row.examinationType+'\',\''+row.examinationId+'\')">修改</a>'
                     // let detail='<a href="javascript:void(0);" onclick="fetchDetail(\''+row.id+'\')">详情</a>'
-                    return d+" "+m/*+" "+detail*/
+                    return d/*+" "+m*//*+" "+detail*/
                 }
             }
          ]
     })
 }
 
-
-function modifyGoods(examinationTitle,examinationA,examinationB,
-                     examinationC,examinationD,examinationAnswer,
-                     examinationScore,examinationDegree,examinationType,examinationId){
-    // var dataJSON={};
-    // dataJSON.examinationId=examinationId;
-    // $.ajax({
-    //     url:'http://localhost:8080/Exam_Jay_SSM/userLogin',
-    //     type:'POST',
-    //     data:JSON.stringify(dataJSON),
-    //     contentType:'application/json;charset=UTF-8'
-    //
-    // })
-    localStorage.setItem("examinationId",examinationId);
-    if (examinationType=="选择题"){
-        //打开 选择题弹出层
-        layer.open({
-            type:2,//可传入的值有：0（信息框，默认）1（页面层）2（iframe层）3（加载层）4（tips层）。
-            title:'商品修改',
-            maxmin:false,
-            shadeClose:false,
-            area:[1000+'px',($(window).height() - 50)+'px'],//弹出层的宽高
-            content:'../main/xuanze-update.html',//设置弹出层打开的页面
-            //弹出层页面成功打开后，的设置
-            success:function(layero,index){
-                //当前是表格页面     修改是表格的子页面   父页面JS代码中将数据传递给子页面中
-                //获取子页面HTML对象
-                let childBody= layer.getChildFrame('body',index);
-
-                //在childBody子页面body区域中find（查找）input标签name属性是xxx的那个input对象，给其设置值为xxx
-                $(childBody).find('textarea[id=xuanzetitle]').val(examinationTitle);
-                $(childBody).find('textarea[id=Atitle]').val(examinationA);
-                $(childBody).find('textarea[id=Btitle]').val(examinationB);
-                $(childBody).find('textarea[id=Ctitle]').val(examinationC);
-                $(childBody).find('textarea[id=Dtitle]').val(examinationD);
-                $(childBody).find('input[name=answer][value='+ examinationAnswer +']').checked="checked";
-                $(childBody).find('input[id=score]').val(examinationScore);
-                $(childBody).find('input[name=degree][value='+examinationDegree+']').checked="checked";
-                //获取子页面JS对象
-            }
-        });
-    }else{
-        //打开 弹出层
-        layer.open({
-            type:2,//可传入的值有：0（信息框，默认）1（页面层）2（iframe层）3（加载层）4（tips层）。
-            title:'商品修改',
-            maxmin:false,
-            shadeClose:false,
-            area:[1000+'px',($(window).height() - 50)+'px'],//弹出层的宽高
-            content:'../main/jianda-update.html',//设置弹出层打开的页面
-            //弹出层页面成功打开后，的设置
-            success:function(layero,index){
-                //当前是表格页面     修改是表格的子页面   父页面JS代码中将数据传递给子页面中
-                //获取子页面HTML对象
-                let childBody= layer.getChildFrame('body',index);
-
-                //在childBody子页面body区域中find（查找）input标签name属性是xxx的那个input对象，给其设置值为xxx
-                $(childBody).find('textarea[id=jiandatitle]').val(examinationTitle);
-                $(childBody).find('textarea[id=answer]').val(examinationAnswer);
-                $(childBody).find('input[id=score]').val(examinationScore);
-                $(childBody).find('input[name=degree]').val(examinationDegree);
-                //获取子页面JS对象
-            }
-        });
-    }
-}
-
-
-
-function removeData(examinationTitle){
-    if(examinationTitle){
-        let url="http://localhost:8080/Exam_Jay_SSM/deleteEx"
-        var dataJSON={};
-
-        dataJSON.examinationTitle=examinationTitle;
-        $.ajax({
-            url:url,
-            data:JSON.stringify(dataJSON),
-            dataType:'json',
-            contentType:'application/json;charset=UTF-8',
-            type:'POST',
-            success:function(result){
-                console.log(result)
-                //将返回的result数据，渲染到页面上
-                if(result==1){
-                    //删除成功-->重新渲染表格的数据
-                    reLoad()
-                }else{
-                    alert("删除失败");
-                }
-            },
-            error:function(result){
-            }
-        })
-
-    }else{
-        alert("数据有问题！无法删除");
-    }
+function lookData(userName,paperId,userId,score,accuracy) {
+    localStorage.setItem("yuejuan-username",userName)
+    localStorage.setItem("yuejuan-paperId",paperId)
+    localStorage.setItem("yuejuan-userid",userId)
+    localStorage.setItem("yuejuan-score",score)
+    localStorage.setItem("yuejuan-zql",accuracy)
+    window.location.href="kaoshi-yuejuan.html"
 }
--- bootstrap-table/js/shijuanComtext.js
+++ bootstrap-table/js/shijuanComtext.js
@@ -10,6 +10,7 @@ window.onload=function requestData(){
         data:JSON.stringify(dataJSON),
         contentType:'application/json;charset=UTF-8',
         success: function (data) {
+            console.log(data[0])
             /*这个方法里是ajax发送请求成功之后执行的代码*/
             showData(data);//我们仅做数据展示
         },
@@ -19,47 +20,23 @@ window.onload=function requestData(){
     });
 }
 
-function but(){
-    // var dataJSON={}
-    // dataJSON.userAcc=$("#acc").val();
-    // $.ajax({
-    //     url:'http://localhost:8080/Exam_Jay_SSM/userLogin',
-    //     type:'POST',
-    //     data:JSON.stringify(dataJSON),
-    //     contentType:'application/json;charset=UTF-8',
-    //     success:function(result){
-    //         console.log(result)
-    //         if (result==1) {
-    //             // alert("登录成功！")
-    //             window.location.href="main/index.html"
-    //         }
-    //         if (result==2){
-    //             alert("账号不能为空或者账号密码有误，请重试！")
-    //         }
-    //
-    //     },
-    //     error:function(){
-    //
-    //     }
-    // });
 
-}
 
 //展示数据
 function showData(data) {
-    var str = "";//定义用于拼接的字符串
+    // var str = "";//定义用于拼接的字符串
     for (var i = 0; i < data.length; i++) {
         //拼接表格的行和列
         if (data[i].papercType=="选择题"){
             var s=i+1;
-            str = "<tr><td>"+s+"</td>" + "<td>"+"<b>[</b>"+"<b>"+data[i].papercType+"</b>" +"<b>]</b>"+ "&nbsp;" + data[i].papercTitle+ "&nbsp;"+"("+data[i].papercScore+")"+"<br>"
+            var str = "<tr><td>"+s+"</td>" + "<td>"+"<b>[</b>"+"<b>"+data[i].papercType+"</b>" +"<b>]</b>"+ "&nbsp;" + data[i].papercTitle+ "&nbsp;"+"("+data[i].papercScore+")"+"<br>"
                +"<font color='gray'size='1'>题目难度:</font>" +"<font color='gray'size='1'>"+data[i].papercDegree+"</font>"+"<br>" +"A:"+data[i].papercA+"<br>"+"B:"+data[i].papercB+"<br>"+"C:"+data[i].papercC+"<br>"+"D:"+data[i].papercD+"<br>"
                 +"正确答案:"+data[i].papercRightanswer+"</td>"+"<td>"+"<input type='checkbox' name='ti' >"+"<span hidden>"+data[i].papercId+"</span>"+"</td>"+"</tr>";
             //追加到table中
             $("#tab").append(str);
         }else{
             var s=i+1;
-            str = "<tr><td>"+s+"</td>" + "<td>"+"<b>[</b>"+"<b>"+data[i].papercType+"</b>" +"<b>]</b>"+ "&nbsp;" + data[i].papercTitle+ "&nbsp;"+"("+data[i].papercScore+")"+"<br>"
+            var str = "<tr><td>"+s+"</td>" + "<td>"+"<b>[</b>"+"<b>"+data[i].papercType+"</b>" +"<b>]</b>"+ "&nbsp;" + data[i].papercTitle+ "&nbsp;"+"("+data[i].papercScore+")"+"<br>"
                 +"<font color='gray'size='1'>题目难度:</font>" +"<font color='gray'size='1'>"+data[i].papercDegree+"</font>"+"<br>"
                 +"正确答案:"+data[i].papercRightanswer+"</td>"+"<td>"+"<input type='checkbox' name='ti' >"+"<span hidden>"+data[i].papercId+"</span>"+"</td>"+"</tr>";
             //追加到table中
